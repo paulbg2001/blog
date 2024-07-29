9ca4fdc2b970760e74d6cf1114be32b2adf35e9a
paulbg2001 <paul.baragraeff@ulbsibiu.ro>
2024-07-29 19:21:31 +0300

    commit test

--- public/script.js
+++ public/script.js
@@ -8,18 +8,27 @@ document.addEventListener('DOMContentLoaded', () => {
       const response = await fetch(`/api/posts${query ? `?q=${query}` : ''}`);
       const posts = await response.json();
       if (Array.isArray(posts)) {
-        postsDiv.innerHTML = posts.map(post => `
-          <div class="col-md-4 mb-4">
-            <div class="card" style="width: 18rem;">
-              ${post.image_path ? `<img src="${post.image_path}" class="card-img-top" alt="${post.title}">` : ''}
-              <div class="card-body">
-                <h5 class="card-title"><strong>${post.title}</strong></h5>
-                <p class="card-text">${post.short_text}</p>
-                <a href="/post/${post.id}" class="btn btn-primary">Read More</a>
+        postsDiv.innerHTML = posts.map(post => {
+          // Convertirea datei într-un format simplu (fără ora)
+          const formattedDate = new Date(post.created_at).toLocaleDateString('en-GB', {
+            year: 'numeric',
+            month: '2-digit',
+            day: '2-digit'
+          });
+          return `
+            <div class="col-md-4 mb-4">
+              <div class="card" style="width: 18rem;">
+                ${post.image_path ? `<img src="${post.image_path}" class="card-img-top" alt="${post.title}">` : ''}
+                <div class="card-body">
+                  <h5 class="card-title"><strong>${post.title}</strong></h5>
+                  <p class="card-text">${post.short_text}</p>
+                  <p class="card-text">${formattedDate}</p>
+                  <a href="/post/${post.id}" class="btn btn-primary">Read More</a>
+                </div>
               </div>
             </div>
-          </div>
-        `).join('');
+          `;
+        }).join('');
       } else {
         console.error('Unexpected response format', posts);
       }
@@ -28,6 +37,7 @@ document.addEventListener('DOMContentLoaded', () => {
     }
   };
 
+  // Încarcă toate postările la început
   loadPosts();
 
   // Adaugă un event listener pentru search input
